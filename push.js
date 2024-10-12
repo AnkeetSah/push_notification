a9bfd0c8d5dcf45cc5c67fdf4b989b793dbfe07c
AnkeetSah <ankeetkumar7777@gmail.com>
2024-10-12 11:56:13 +0530

    second commit

--- push.js
+++ push.js
@@ -1,15 +1,25 @@
-let push =require('web-push');
+let push = require("web-push");
 
-let vapidKeys={
-    publicKey: 'BA4MxFbqv-Tx-bZ9M6s_mNt9LstHI_jFWgC-JMOEANJnujrmqhCBgDpp2fvZE2GeksT9uHdl3Qf08ppYSqkxBMc',
-    privateKey: 'gCAJHsCVR3NMibfZLfRGtws2hw9b6TB_T1ZHUS5dz_Y'
-  }
+let vapidKeys = {
+  publicKey:
+    "BA4MxFbqv-Tx-bZ9M6s_mNt9LstHI_jFWgC-JMOEANJnujrmqhCBgDpp2fvZE2GeksT9uHdl3Qf08ppYSqkxBMc",
+  privateKey: "gCAJHsCVR3NMibfZLfRGtws2hw9b6TB_T1ZHUS5dz_Y",
+};
 
 push.setVapidDetails(
-    'mailto:example@yourdomain.org',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
-  );
+  "mailto:example@yourdomain.org",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 
-  let sub={};
-  push.sendNotification(sub,'test message')
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/d8IwVTqEaaw:APA91bG9jC4Nv6diwksQWfyYz-mAG-gvXY97MfGHRZlqhDk73sfv9Y6rKL0BAMUwqSJgVq8vV8-djitqAq6huogkTKJJ538L7ihCvztRV26zHEdc1Hxdb_72djSSDitsuWypOrv8zWvH",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BNizZXVUZB62DLpIiDcaHwmwjy8PCTHuj1UEofPgLOkg2F5ur1RfIbTnYfJLsKy_Y5QcKVq2yeO2RNjI3kJjE-4",
+    auth: "nS54yFcMCe17-AEplZ6qqg",
+  },
+};
+push.sendNotification(sub, "test message");
--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('push', () => {
-    self.registration.showNotification('Hello world!', {});
+self.addEventListener('push', event => {
+    const data = event.data?.json() || {};
+  
+    const options = {
+      body: data.body || 'You have a new message!',
+      icon: data.icon || '/path/to/default-icon.png',
+      badge: data.badge || '/path/to/badge-icon.png',
+      tag: data.tag || 'notification',
+      actions: [
+        { action: 'open_app', title: 'Open App', icon: '/path/to/open-icon.png' },
+        { action: 'dismiss', title: 'Dismiss', icon: '/path/to/dismiss-icon.png' }
+      ],
+      vibrate: [200, 100, 200],
+      requireInteraction: true
+    };
+  
+    event.waitUntil(
+      self.registration.showNotification(data.title || 'Notification', options)
+    );
   });
+  
+  self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    if (event.action === 'open_app') clients.openWindow('/');
+  });
+  
