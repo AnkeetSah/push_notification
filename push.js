d43c72d84e1231a2b0889652c61d26b0221f2daf
AnkeetSah <ankeetkumar7777@gmail.com>
2024-10-12 21:12:54 +0530

    updated whole cod12

--- push.js
+++ push.js
@@ -17,12 +17,12 @@ push.setVapidDetails(
 // Your push subscription details
 const sub = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/cJkGIwHK6QY:APA91bE-2WWTyqJ0pprB5kZ4Mm_qd7qtuAa1f9FS9BAH3mdQmcgovVUW2JWxE5oR-8Ssro2E0n1P_UEKjOcuXSDHp90mDxnfz8GNYIXAN9iHRd2OaZJVJtzG_QZGNeCWIYs7fpLWlp4y",
+    "https://fcm.googleapis.com/fcm/send/dzY8uFheDCo:APA91bEmO6fT-WyoCsPNBjgf0NkUA_5t8L6ggQXyDG5DcbrAQkDC__Hbtgb256Ga7kIwYsEC_AjVzUamMFl-7gAS0fJncadhX-_gg-r4zR28HdzLVmKFmcWUOGr1h0Iqj88MWcw2OOKZ",
   expirationTime: null,
   keys: {
     p256dh:
-      "BINE5DUqN_mFJlAt3uMb1LcGhH-usrPZWWWwXIMp3nSPw1KAFa1GRuamzFJ1Rl-vTEYxkXHrApP66oe6DaY5N5s",
-    auth: "gHdcVCtzleN0tvAS2hSlKA",
+      "BHscYMc0A2lG13rRlUCb0XAWvXEyQrUoTNW-N-jjZrEo6vdNV4t7l56PFsMsAhOhA1ilveqaXCYWhwcMVKFUzWk",
+    auth: "L9RPMb5KkBgVEPoyQJ_7OA",
   },
 };
 
