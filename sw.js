5f2c72e83c855a72a1e5c783ed9172b30fe3fffe
AnkeetSah <ankeetkumar7777@gmail.com>
2024-10-12 17:02:56 +0530

    updated whole cod10

--- sw.js
+++ sw.js
@@ -16,11 +16,32 @@ self.addEventListener('push', (event) => {
     const options = {
         body: data.message,
         icon: 'https://img.icons8.com/ios/452/notification.png',
-        badge: 'https://img.icons8.com/ios/452/badge.png'
+        badge: 'https://img.icons8.com/ios/452/badge.png',
+        actions: [
+            {
+                action: 'youtube-action',
+                title: 'Go to YouTube',
+                icon: 'https://img.icons8.com/ios-filled/50/000000/youtube-play.png'
+            }
+        ],
+        data: {
+            url: 'https://www.youtube.com', // Redirect URL on click
+        }
     };
 
     event.waitUntil(
         self.registration.showNotification('Push Notification', options)
+    );
+});
+
+self.addEventListener('notificationclick', (event) => {
+    console.log('Notification click received:', event.notification);
+
+    event.notification.close(); // Close the notification when clicked
+
+    // Handle the click and redirect to YouTube or any URL set in options.data
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
     );
 });
 
@@ -30,7 +51,10 @@ self.addEventListener('activate', (event) => {
         self.registration.showNotification('Test Notification', {
             body: 'This is a test notification triggered manually.',
             icon: 'https://img.icons8.com/ios/452/notification.png',
-            badge: 'https://img.icons8.com/ios/452/badge.png'
+            badge: 'https://img.icons8.com/ios/452/badge.png',
+            data: {
+                url: 'https://www.youtube.com' // Redirect URL on click
+            }
         })
     );
 });
