b9645057f144321a64f538fed2a09f65cb7b8666
AnkeetSah <ankeetkumar7777@gmail.com>
2024-10-12 16:01:03 +0530

    updated whole cod8

--- push.js
+++ push.js
@@ -1,35 +1,39 @@
-const push = require('web-push');
+const push = require("web-push");
 
 // VAPID keys for authentication
 const vapidKeys = {
-  publicKey: 'BA4MxFbqv-Tx-bZ9M6s_mNt9LstHI_jFWgC-JMOEANJnujrmqhCBgDpp2fvZE2GeksT9uHdl3Qf08ppYSqkxBMc',
-  privateKey: 'gCAJHsCVR3NMibfZLfRGtws2hw9b6TB_T1ZHUS5dz_Y'
+  publicKey:
+    "BA4MxFbqv-Tx-bZ9M6s_mNt9LstHI_jFWgC-JMOEANJnujrmqhCBgDpp2fvZE2GeksT9uHdl3Qf08ppYSqkxBMc",
+  privateKey: "gCAJHsCVR3NMibfZLfRGtws2hw9b6TB_T1ZHUS5dz_Y",
 };
 
 // Set VAPID details
 push.setVapidDetails(
-  'mailto:example@yourdomain.org', // Replace with your email
+  "mailto:example@yourdomain.org", // Replace with your email
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
 // Your push subscription details
 const sub = {
-  endpoint: 'https://fcm.googleapis.com/fcm/send/fjgoMRmmCJE:APA91bEcTntW5Vnj529XmmqJ8pTESPtf2DCq-8iyaj0LQXrPD_b1dWoCUxYEV3BrojRHMcpRb3rUQaOZBJkhhOBwOWMkbz-6beiM5Ge1qWJl-ss21KM5-MceHqzWdK2qVH_VeRvlPNnU',
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/eJLyHI4mOQc:APA91bHNlYJWOjN9F0lYfiyqLtFH9OvaWKBw3p8oKBAUhAi1ECBcDxB_d93Ws42-nZDMthRm86kGxC-h5YPsXtbY3cRp0VbSABz4CMX0KpQSuXV25LMfgNqD-TanwTZ55U5XFn_5thMd",
   expirationTime: null,
   keys: {
-    p256dh: 'BMw-4eut15iiuY-31ANbQyx-SULkz1lOneI3oE2KVZptrtXxxlvF0cdgOj9P8HWBlEKYDPmOj3lcudxp2z_ddL8',
-    auth: 'loIPm6lFw5cC367X5OEgEQ'
-  }
+    p256dh:
+      "BA4H1bp_bEwln8-GzmDrDMxcNuuUh7lm85Ma7NXieN-_SUplXvjSOlYJ8ZBAPO1YdHW3MsBLLSt3642S6KERQUI",
+    auth: "KeiUYK6yy4HoI3foX_ZZjA",
+  },
 };
 
 // Prepare a JSON payload
-const payload = JSON.stringify({ message: 'This is a test notification!' });
+const payload = JSON.stringify({ message: "This is a test notification!" });
 
-push.sendNotification(sub, payload)
+push
+  .sendNotification(sub, payload)
   .then((response) => {
-    console.log('Push notification sent successfully', response);
+    console.log("Push notification sent successfully", response);
   })
   .catch((error) => {
-    console.error('Error sending push notification', error);
+    console.error("Error sending push notification", error);
   });
